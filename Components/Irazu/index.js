c7bab41b3dd01e15d1314e8d3eb3cfc7081e4b13
Jonathan Garcia <jona03g97@gmail.com>
2022-12-26 08:20:39 -0600

    Add: Irazu installartons

---
+++ Components/Irazu/index.js
@@ -0,0 +1,12 @@
+import styles from "./styles";
+
+const Irazu = () => {
+  return (
+    <>
+      <section></section>
+      <style jsx>{styles}</style>
+    </>
+  )
+}
+
+export default Irazu
---
+++ Components/Irazu/styles.js
@@ -0,0 +1,13 @@
+import css from 'styled-jsx/css'
+import { outline } from '../../theme'
+
+const styles = css`
+  section {
+    outline: ${outline};
+    background-color: white;
+    width: 150px;
+    height: 150px;
+  }
+`
+
+export default styles
